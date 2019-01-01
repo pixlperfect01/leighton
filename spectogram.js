7440c5e805e1ecfaf46cee0f6debebfd2316d3aa
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-01 14:45:44 -0600

    Update spectogram.js

--- spectogram.js
+++ spectogram.js
@@ -1,1 +1,25 @@
-function spect(min, max, type){
+//0:heart
+
+
+/*
+   000  000
+  0   00   0
+*/
+function heart(){
+  if(!type)
+    type=0;
+  var final;
+  var range = max - min - 1;
+  var arr=[];
+  arr.length=range/interval;
+  for(var i=0;i<arr.length;i++){
+    arr[i]=[];
+    arr[i].length=arr.length;
+  }
+  switch(type){
+    case 0:
+      
+    break;
+  }
+  return final;
+}
