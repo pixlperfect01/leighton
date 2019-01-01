79e9ef59c0b7b0233615c6fa166131588f0e4a64
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-01 12:49:53 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var fireworks = [];
 var gravity;
 
 function setup() {
-  createCanvas(400, 300);
+  createCanvas(window.innerWidth, window.innerHeight);
   colorMode(HSB);
   gravity = createVector(0, 0.2);
   stroke(255);
